2f7fd3c907d5da44d66a5608c6f865d453abe464
Norin <norin.hor@gmail.com>
2025-11-28 10:29:28 +0900

    purge

--- emacs/lisp/init-preload-local.el
+++
@@ -1,7 +0,0 @@
-;;; init-preload-local.el --- Provide optional preload local variables  -*- lexical-binding: t -*-
-;;; Commentary:
-;;; Code:
-
-(provide 'init-preload-local)
-
-;;; init-preload-local.el ends here
--- nvim/lua/plugin-configs/mason.lua
+++
@@ -1,6 +0,0 @@
-require("mason-nvim-dap").setup {
-	ensure_installed = {},
-	automatic_installation = true,
-}
-
-require "plugin-configs.nvim-dap"
